89f2fcdcd4a4c4e631398bd0cea46aa42aab2545
Param <paramvirs25@gmail.com>
2023-05-02 14:17:16 +0530

    Refactor method to call constructor only on null

--- PHP/Courses.php
+++ PHP/Courses.php
@@ -14,44 +14,58 @@ class Courses {
     const APPRENTICE_BASIC_IW = "apprentice_basic_iw";
     const APPRENTICE_COACH = "apprentice_coach";
 
-    private static function basic_iw_1() {
-        return new Courses("Basic inner work 1", self::BASIC_IW_1, "Basic IW 1"); 
+    private static $basic_iw_1 = null;
+    public static function getBasicIw1()
+    {
+        return self::$basic_iw_1 ?? self::$basic_iw_1 = new Courses("Basic inner work 1", self::BASIC_IW_1, "Basic IW 1");
     }
-    
-    private static function tfciw() {
-        return new Courses("Basic inner work 2", self::TFCIW, "Basic IW 2"); 
+
+    private static $tfciw = null;
+    public static function getTfciw()
+    {
+        return self::$tfciw ?? self::$tfciw = new Courses("Basic inner work 2", self::TFCIW, "Basic IW 2");
     }
-    
-    private static function tfcaiw1() {
-        return new Courses("Advanced inner work 1", self::TFCAIW1, "Advanced IW 1"); 
+
+    private static $tfcaiw1 = null;
+    public static function getTfcaiw1()
+    {
+        return self::$tfcaiw1 ?? self::$tfcaiw1 = new Courses("Advanced inner work 1", self::TFCAIW1, "Advanced IW 1");
     }
-    
-    private static function advanced_tf_healings_1() {
-        return new Courses("Advanced Twin Flame Healings 1", self::ADV_TF_HEALINGS_1, "Advanced TF Healings 1"); 
+
+    private static $advanced_tf_healings_1 = null;
+    public static function getAdvancedTfHealings1()
+    {
+        return self::$advanced_tf_healings_1 ?? self::$advanced_tf_healings_1 = new Courses("Advanced Twin Flame Healings 1", self::ADV_TF_HEALINGS_1, "Advanced TF Healings 1");
     }
-    
-    private static function chakra_healing_balancing() {
-        return new Courses("Chakra balancing program", self::CHAKRA_HEALING_BALANCING, "Chakra balancing 1"); 
+
+    private static $chakra_healing_balancing = null;
+    public static function getChakraHealingBalancing()
+    {
+        return self::$chakra_healing_balancing ?? self::$chakra_healing_balancing = new Courses("Chakra balancing program", self::CHAKRA_HEALING_BALANCING, "Chakra balancing 1");
     }
-    
-    private static function certified_coach() {
-        return new Courses("Twin Flames Coach program", self::CERTIFIED_COACH, "Twin Flames Coach 1"); 
+
+    private static $certified_coach = null;
+    public static function getCertifiedCoach()
+    {
+        return self::$certified_coach ?? self::$certified_coach = new Courses("Twin Flames Coach program", self::CERTIFIED_COACH, "Twin Flames Coach 1");
     }
-    
-    private static function certified_yoga_teacher() {
-        return new Courses("Yoga Teacher Training program", self::CERTIFIED_YOGA_TEACHER, "Yoga Teacher Training"); 
+
+    private static $certified_yoga_teacher = null;
+    public static function getCertifiedYogaTeacher()
+    {
+        return self::$certified_yoga_teacher ?? self::$certified_yoga_teacher = new Courses("Yoga Teacher Training program", self::CERTIFIED_YOGA_TEACHER, "Yoga Teacher Training");
     }
-    
-    private static function shakti_kawach() {
-        return new Courses("Shakti Kavach program", self::SHAKTI_KAWACH, "Shakti Kavach"); 
+
+    private static $shakti_kawach = null;
+    public static function getShaktiKawach()
+    {
+        return self::$shakti_kawach ?? self::$shakti_kawach = new Courses("Shakti Kavach program", self::SHAKTI_KAWACH, "Shakti Kavach");
     }
-    
-    private static function apprentice_basic_iw() {
-        return new Courses("Apprentice - Basic IW", self::APPRENTICE_BASIC_IW, "Apprenticeship Basic IW"); 
-    }
-    
-    private static function apprentice_coach() {
-        return new Courses("Apprentice - Twin Flames Coach", self::APPRENTICE_COACH, "Apprenticeship TF Coach"); 
+
+    private static $apprentice_basic_iw = null;
+    public static function getApprenticeBasicIw()
+    {
+        return self::$apprentice_basic_iw ?? self::$apprentice_basic_iw = new Courses("Apprentice - Basic IW", self::APPRENTICE_BASIC_IW, "Apprenticeship Basic IW");
     }
     
     public $courseName;
