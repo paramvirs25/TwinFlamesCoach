d75349c10d81500650459cbd730c89d34355aa4c
Param <paramvirs25@gmail.com>
2023-05-17 20:14:05 +0530

    bring focus to search box

--- Javascript/TubeBuddySearch.js
+++ Javascript/TubeBuddySearch.js
@@ -39,6 +39,8 @@ function cannedResponseClicked(element) {
           });
         }
       });
+
+      document.querySelector('#searchCannedResponse-' + qTipId).focus();
     } catch (e) {
       console.log(e);
     }
