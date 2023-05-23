1cdf9b3c2f9f0cae0b14970e20f24407c0f2521e
Param <paramvirs25@gmail.com>
2023-05-23 21:17:03 +0530

    Added search elements in div

--- PHP/Playlist Search - Feeds for YouTube.php
+++ PHP/Playlist Search - Feeds for YouTube.php
@@ -5,8 +5,11 @@
 	.showThumb {
       display: block!important;
     }
+	.searchElements{
+		text-align: center;
+	}
 	.searchPlaylist{
-		width:180px;
+		width:100%;
 		margin-left: 5px;
 		padding-top: 0px;
 		background-image:url('images/search.jpg');
@@ -18,97 +21,120 @@
 		position:relative;
 		right:20px;    
 	}
+
+	.playlistVideoCount{
+		font-size: 12px;
+		font-weight: bold;
+	}
 </style>
 
 <script>
-	function searchPlaylist(){
-		// Get all search boxes on page
-		var searchInputs = document.getElementsByClassName("searchPlaylist");
-		
-		var searchText = searchInputs[0].value.toLowerCase();
-		var titles = document.getElementsByClassName("epyt-gallery-title");
+	class FeedsForYT_TFC {
+		constructor() {
+			this.searchInputs = null;
+			this.titles = null;
+			this.thumbCounter = 0;
+		}
 
-		//remove all row breaks
-		removeRowBreaks();
+		showFoundVideosCount(totalVisibleVideos){
+			// Get the element with class "playlistVideoCount"
+			var divElement = document.querySelector(".playlistVideoCount");
+			divElement.textContent = 'Found ' + totalVisibleVideos + ' Video(s)';
+		}
 
-		var thumbCounter = 0;
-		
-		// Loop through each title and check for a match with search text
-		// Note: If there ar emultiple search boxes on a page, then this loop will sacn through 
-		// titles corresponding to all search boxes. 
-		for (var i = 0; i < titles.length; i++) {
-			var titleText = titles[i].textContent.toLowerCase();
-			var parentDiv = titles[i].parentNode;
-			
-			//if match
-			if (titleText.includes(searchText)) {
-				//show thumbnail
-				parentDiv.classList.remove("hideThumb");
-				parentDiv.classList.add("showThumb");
-				thumbCounter++;
+		searchPlaylist() {
+			// Get all search boxes on the page
+			this.searchInputs = document.getElementsByClassName("searchPlaylist");
+			var searchText = this.searchInputs[0].value.toLowerCase();
+			this.titles = document.getElementsByClassName("sby_video_title");
 
-				//for every two thumbs insert a row break;
-				if (thumbCounter % 2 === 0) {
-					addRowBreaks(parentDiv);
+			var visibleVideos = 0;
+			// Loop through each title and check for a match with the search text
+			for (var i = 0; i < this.titles.length; i++) {
+				var titleText = this.titles[i].textContent.toLowerCase();
+				var parentDiv = this.titles[i].closest('.sby_item');
+
+				if (titleText.includes(searchText)) {
+					// Show thumbnail
+					parentDiv.style.display = '';
+					//parentDiv.classList.remove("hideThumb");
+					//parentDiv.classList.add("showThumb");
+
+					visibleVideos++;
+					
+				} else {
+					// Hide thumbnail
+					parentDiv.style.display = 'none';
+					//parentDiv.classList.remove("showThumb");
+					//parentDiv.classList.add("hideThumb");
 				}
-			} else {
-				//hide thumbnail
-				parentDiv.classList.remove("showThumb");
-				parentDiv.classList.add("hideThumb");
 			}
 
-			//reset thumbCounter when all titles corresponding to a search box are scanned.
-			//This helps in having correct row breaks if page contains multiple search boxes.
-			if(i == (titles.length/searchInputs.length) - 1){
-				thumbCounter = 0;
-			}
-      	}
+			this.showFoundVideosCount(visibleVideos);
+		}
+
+		showVideoTitle() {
+			// Get all div elements with class "sby_thumbnail_hover"
+			var divsToMove = document.querySelectorAll('.sby_thumbnail_hover');
+
+			var visibleVideos = 0;
+
+			// Loop through each div and move it next to its parent element
+			divsToMove.forEach(function (div) {
+				var parentElement = div.parentNode;
+				parentElement.parentNode.insertBefore(div, parentElement.nextSibling);
+				visibleVideos++;
+			});
+
+			this.showFoundVideosCount(visibleVideos);
+
+			// Remove all CSS classes from the moved divs
+			divsToMove.forEach(function (div) {
+				div.className = '';
+			});
+		}
+
+		initYTPlayListSearch() {
+
+			var searchElements = document.createElement("div");
+			//searchElements.align = "center";
+			searchElements.className = "searchElements";
+
+			// Create the input element
+			var inputElement = document.createElement("input");
+			inputElement.className = "searchPlaylist";
+			inputElement.type = "search";
+			inputElement.placeholder = "Search all channel videos";
+			inputElement.setAttribute("oninput", "feeds.searchPlaylist();");
+			searchElements.appendChild(inputElement);
+
+			//Create span for video count
+			var playlistVideoCountElement = document.createElement("span");
+			playlistVideoCountElement.className = "playlistVideoCount";
+			searchElements.appendChild(playlistVideoCountElement);
+
+			// Get the div with class "sby_items_wrap"
+			var divElement = document.querySelector(".sby_items_wrap");
+			divElement.parentNode.insertBefore(searchElements, divElement);
+
+			// Insert the input element before the div
+			//divElement.parentNode.insertBefore(inputElement, divElement);
+			
+			// Insert the span element after the input element
+			//inputElement.parentNode.insertBefore(playlistVideoCountElement, inputElement.nextSibling);
+
+			this.showVideoTitle();
+		}
 	}
 
-	function addRowBreaks(element) {
-		var div = document.createElement("div");
-		div.classList.add("epyt-gallery-rowbreak");
-		
-		element.insertAdjacentElement("afterend", div);
-	}
+	// Create an instance of the FeedsForYT_TFC class
+	var feeds = new FeedsForYT_TFC();
 
-	
-
-	function showVideoTitle(){
-		// Get all div elements with class "sby_thumbnail_hover"
-		var divsToMove = document.querySelectorAll('.sby_thumbnail_hover');
-
-		// Loop through each div and move it next to its parent element
-		divsToMove.forEach(function(div) {
-			var parentElement = div.parentNode;
-			parentElement.parentNode.insertBefore(div, parentElement.nextSibling);
-		});
-
-		// Remove all CSS classes from the moved divs
-		divsToMove.forEach(function(div) {
-			div.className = '';
-		});
-
-	}
-
-	function initYTPlayListSearch() {
-		showVideoTitle();
-
-		// Create the input element
-		var inputElement = document.createElement("input");
-		inputElement.className = "searchPlaylist";
-		inputElement.type = "search";
-		inputElement.placeholder = "Search video";
-		inputElement.setAttribute("oninput", "searchPlaylist();");
-
-		// Get the div with class "sby_items_wrap"
-		var divElement = document.querySelector(".sby_items_wrap");
-	
-		// Insert the input element before the div
-		divElement.parentNode.insertBefore(inputElement, divElement);
-	}
-
-	window.addEventListener('load', () => {		
-		initYTPlayListSearch();
+	// Call initYTPlayListSearch() on page load
+	window.addEventListener('load', function () {
+		feeds.initYTPlayListSearch();
 	});
+
 </script>
+<!--<input class="searchPlaylist" type = "search" placeholder = "Search video" oninput="feeds.searchPlaylist();">
+<span class="playlistVideoCount"></span>-->
