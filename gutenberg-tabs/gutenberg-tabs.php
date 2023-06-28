2b9a1589677a1b82b59616d020728811911c26f0
Param <paramvirs25@gmail.com>
2023-06-28 23:51:40 +0530

    Files enqued

--- gutenberg-tabs/gutenberg-tabs.php
+++ gutenberg-tabs/gutenberg-tabs.php
@@ -6,12 +6,19 @@ Version: 1.0.0
 Author: Your Name
 */
 
-// Enqueue the plugin stylesheets and scripts
+// Enqueue the plugin stylesheets and scripts for the block editor
 function gutenberg_tabs_enqueue_assets() {
     wp_enqueue_style( 'gutenberg-tabs-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.0.0' );
     wp_enqueue_script( 'gutenberg-tabs-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'wp-blocks', 'wp-element', 'wp-editor' ), '1.0.0', true );
 }
 add_action( 'enqueue_block_editor_assets', 'gutenberg_tabs_enqueue_assets' );
+
+// Enqueue the plugin stylesheets and scripts for front-end
+function gutenberg_tabs_enqueue_front_assets() {
+    wp_enqueue_style( 'gutenberg-tabs-front-style', plugin_dir_url( __FILE__ ) . 'css/style.css', array(), '1.0.0' );
+    wp_enqueue_script( 'gutenberg-tabs-front-script', plugin_dir_url( __FILE__ ) . 'js/script.js', array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'gutenberg_tabs_enqueue_front_assets' );
 
 // Register the tab block
 function gutenberg_tabs_register_block() {
--- gutenberg-tabs/js/script.js
+++ gutenberg-tabs/js/script.js
@@ -1,10 +1,11 @@
-(function(blocks, editor, components, i18n, element) {
+(function (blocks, editor, components, i18n, element) {
     var el = element.createElement;
     var registerBlockType = blocks.registerBlockType;
     var RichText = editor.RichText;
     var Fragment = element.Fragment;
     var TextControl = components.TextControl;
     var InspectorControls = editor.InspectorControls;
+    var TabPanel = components.TabPanel;
 
     registerBlockType('gutenberg-tabs/tab', {
         title: i18n.__('Tab'),
@@ -20,11 +21,11 @@
             },
         },
 
-        edit: function(props) {
+        edit: function (props) {
             var tabs = props.attributes.tabs;
 
             function updateTabTitle(value, index) {
-                var newTabs = tabs.map(function(tab, tabIndex) {
+                var newTabs = tabs.map(function (tab, tabIndex) {
                     if (index === tabIndex) {
                         return { ...tab, title: value };
                     }
@@ -35,7 +36,7 @@
             }
 
             function updateTabContent(value, index) {
-                var newTabs = tabs.map(function(tab, tabIndex) {
+                var newTabs = tabs.map(function (tab, tabIndex) {
                     if (index === tabIndex) {
                         return { ...tab, content: value };
                     }
@@ -46,96 +47,100 @@
             }
 
             function addNewTab() {
-                var newTabs = [ ...tabs, { title: '', content: '' } ];
+                var newTabs = [...tabs, { title: '', content: '' }];
                 props.setAttributes({ tabs: newTabs });
             }
 
             function removeTab(index) {
-                var newTabs = tabs.filter(function(tab, tabIndex) {
+                var newTabs = tabs.filter(function (tab, tabIndex) {
                     return index !== tabIndex;
                 });
 
                 props.setAttributes({ tabs: newTabs });
             }
 
-            return el(Fragment, null,
-                el(InspectorControls, null,
-                    el(components.PanelBody, { title: i18n.__('Tabs') },
-                        el(components.Button, {
-                            onClick: addNewTab,
-                            isPrimary: true
-                        }, i18n.__('Add New Tab'))
-                    )
+            return el(
+                Fragment,
+                null,
+                el(
+                    InspectorControls,
+                    null,
+                    el(components.PanelBody, { title: i18n.__('Tabs') }, el(components.Button, { onClick: addNewTab, isPrimary: true }, i18n.__('Add New Tab')))
                 ),
-                el('div', { className: props.className },
-                    el('ul', { className: 'gutenberg-tabs-tabs' },
-                        tabs.map(function(tab, index) {
-                            return el('li', { key: index },
-                                el(TextControl, {
-                                    value: tab.title,
-                                    onChange: function(value) {
-                                        updateTabTitle(value, index);
-                                    },
-                                    placeholder: i18n.__('Tab Title')
-                                }),
-                                el(components.IconButton, {
-                                    icon: 'no',
-                                    onClick: function() {
-                                        removeTab(index);
-                                    },
-                                    className: 'gutenberg-tabs-remove-button'
-                                })
-                            );
-                        })
-                    ),
-                    el('div', { className: 'gutenberg-tabs-content' },
-                        tabs.map(function(tab, index) {
-                            return el('div', { key: index, className: 'gutenberg-tabs-tab-content' },
-                                el(RichText, {
-                                    tagName: 'h3',
-                                    value: tab.title,
-                                    onChange: function(value) {
-                                        updateTabTitle(value, index);
-                                    },
-                                    placeholder: i18n.__('Tab Title')
-                                }),
-                                el(RichText, {
-                                    tagName: 'div',
-                                    multiline: true,
-                                    value: tab.content,
-                                    onChange: function(value) {
-                                        updateTabContent(value, index);
-                                    },
-                                    placeholder: i18n.__('Tab Content')
-                                })
-                            );
-                        })
-                    )
-                )
+                el(TabPanel, {
+                    className: 'gutenberg-tabs-tabs',
+                    activeClass: 'active-tab',
+                    tabs: tabs.map(function (tab) {
+                        return { name: tab.title, title: tab.title };
+                    }),
+                },
+                function (selectedTab) {
+                    return el(
+                        'div',
+                        { className: props.className },
+                        el(
+                            'div',
+                            { className: 'gutenberg-tabs-content' },
+                            tabs.map(function (tab, index) {
+                                return el(
+                                    'div',
+                                    { key: index, className: 'gutenberg-tabs-tab-content' + (selectedTab === tab.title ? ' active-tab' : '') },
+                                    el(RichText, {
+                                        tagName: 'h3',
+                                        value: tab.title,
+                                        onChange: function (value) {
+                                            updateTabTitle(value, index);
+                                        },
+                                        placeholder: i18n.__('Tab Title'),
+                                    }),
+                                    el(RichText, {
+                                        tagName: 'div',
+                                        multiline: true,
+                                        value: tab.content,
+                                        onChange: function (value) {
+                                            updateTabContent(value, index);
+                                        },
+                                        placeholder: i18n.__('Tab Content'),
+                                    })
+                                );
+                            })
+                        )
+                    );
+                })
             );
         },
 
-        save: function(props) {
+        save: function (props) {
             var tabs = props.attributes.tabs;
 
-            return el('div', { className: props.className },
-                el('ul', { className: 'gutenberg-tabs-tabs' },
-                    tabs.map(function(tab, index) {
-                        return el('li', { key: index },
+            return el(
+                'div',
+                { className: props.className },
+                el(
+                    'ul',
+                    { className: 'gutenberg-tabs-tabs' },
+                    tabs.map(function (tab, index) {
+                        return el(
+                            'li',
+                            { key: index },
                             el('span', null, tab.title)
                         );
                     })
                 ),
-                el('div', { className: 'gutenberg-tabs-content' },
-                    tabs.map(function(tab, index) {
-                        return el('div', { key: index, className: 'gutenberg-tabs-tab-content' },
+                el(
+                    'div',
+                    { className: 'gutenberg-tabs-content' },
+                    tabs.map(function (tab, index) {
+                        return el(
+                            'div',
+                            { key: index, className: 'gutenberg-tabs-tab-content' },
                             el('h3', null, tab.title),
                             el('div', { dangerouslySetInnerHTML: { __html: tab.content } })
                         );
                     })
                 )
             );
-        }
+        },
     });
 })(
     window.wp.blocks,
