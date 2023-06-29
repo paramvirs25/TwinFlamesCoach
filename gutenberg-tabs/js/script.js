cc96fa2deb5749e3a750be87ef4ecbc04fe332fe
Param <paramvirs25@gmail.com>
2023-06-29 09:29:01 +0530

    trying new script

--- gutenberg-tabs/js/script.js
+++ gutenberg-tabs/js/script.js
@@ -1,22 +1,25 @@
 (function (blocks, editor, components, i18n, element) {
     var el = element.createElement;
-    var registerBlockType = blocks.registerBlockType;
     var RichText = editor.RichText;
-    var Fragment = element.Fragment;
-    var TextControl = components.TextControl;
-    var InspectorControls = editor.InspectorControls;
     var TabPanel = components.TabPanel;
 
-    registerBlockType('gutenberg-tabs/tab', {
-        title: i18n.__('Tab'),
-        icon: 'screenoptions',
+    blocks.registerBlockType('gutenberg-tabs/tab', {
+        title: i18n.__('Tab', 'gutenberg-tabs'),
+        icon: 'list-view',
         category: 'common',
+
         attributes: {
             tabs: {
                 type: 'array',
                 default: [
-                    { title: 'Tab 1', content: 'Content 1' },
-                    { title: 'Tab 2', content: 'Content 2' },
+                    {
+                        title: i18n.__('Tab 1', 'gutenberg-tabs'),
+                        content: '',
+                    },
+                    {
+                        title: i18n.__('Tab 2', 'gutenberg-tabs'),
+                        content: '',
+                    },
                 ],
             },
         },
@@ -24,89 +27,47 @@
         edit: function (props) {
             var tabs = props.attributes.tabs;
 
-            function updateTabTitle(value, index) {
-                var newTabs = tabs.map(function (tab, tabIndex) {
-                    if (index === tabIndex) {
-                        return { ...tab, title: value };
-                    }
-                    return tab;
-                });
-
-                props.setAttributes({ tabs: newTabs });
-            }
-
-            function updateTabContent(value, index) {
-                var newTabs = tabs.map(function (tab, tabIndex) {
-                    if (index === tabIndex) {
-                        return { ...tab, content: value };
-                    }
-                    return tab;
-                });
-
-                props.setAttributes({ tabs: newTabs });
-            }
-
-            function addNewTab() {
-                var newTabs = [...tabs, { title: '', content: '' }];
-                props.setAttributes({ tabs: newTabs });
-            }
-
-            function removeTab(index) {
-                var newTabs = tabs.filter(function (tab, tabIndex) {
-                    return index !== tabIndex;
-                });
-
+            function onTabChange(newTabs) {
                 props.setAttributes({ tabs: newTabs });
             }
 
             return el(
-                Fragment,
-                null,
-                el(
-                    InspectorControls,
-                    null,
-                    el(components.PanelBody, { title: i18n.__('Tabs') }, el(components.Button, { onClick: addNewTab, isPrimary: true }, i18n.__('Add New Tab')))
-                ),
-                el(TabPanel, {
-                    className: 'gutenberg-tabs-tabs',
+                TabPanel,
+                {
+                    className: props.className,
                     activeClass: 'active-tab',
+                    onSelect: onTabChange,
                     tabs: tabs.map(function (tab) {
-                        return { name: tab.title, title: tab.title };
+                        return {
+                            name: tab.title,
+                            title: tab.title,
+                            className: 'tab-' + tab.title.toLowerCase().replace(' ', '-'),
+                        };
                     }),
                 },
-                function (selectedTab) {
+                function (tab) {
+                    var tabContent = tabs.find(function (t) {
+                        return t.title === tab.name;
+                    });
+
                     return el(
-                        'div',
-                        { className: props.className },
-                        el(
-                            'div',
-                            { className: 'gutenberg-tabs-content' },
-                            tabs.map(function (tab, index) {
-                                return el(
-                                    'div',
-                                    { key: index, className: 'gutenberg-tabs-tab-content' + (selectedTab === tab.title ? ' active-tab' : '') },
-                                    el(RichText, {
-                                        tagName: 'h3',
-                                        value: tab.title,
-                                        onChange: function (value) {
-                                            updateTabTitle(value, index);
-                                        },
-                                        placeholder: i18n.__('Tab Title'),
-                                    }),
-                                    el(RichText, {
-                                        tagName: 'div',
-                                        multiline: true,
-                                        value: tab.content,
-                                        onChange: function (value) {
-                                            updateTabContent(value, index);
-                                        },
-                                        placeholder: i18n.__('Tab Content'),
-                                    })
-                                );
-                            })
-                        )
+                        RichText,
+                        {
+                            tagName: 'div',
+                            className: 'gutenberg-tabs-tab-content',
+                            value: tabContent.content,
+                            onChange: function (newContent) {
+                                var newTabs = tabs.map(function (t) {
+                                    if (t.title === tabContent.title) {
+                                        return Object.assign({}, t, { content: newContent });
+                                    }
+                                    return t;
+                                });
+                                onTabChange(newTabs);
+                            },
+                        }
                     );
-                })
+                }
             );
         },
 
@@ -133,7 +94,11 @@
                     tabs.map(function (tab, index) {
                         return el(
                             'div',
-                            { key: index, className: 'gutenberg-tabs-tab-content' },
+                            {
+                                key: index,
+                                className: 'gutenberg-tabs-tab-content',
+                                style: { display: index === 0 ? 'block' : 'none' },
+                            },
                             el('h3', null, tab.title),
                             el('div', { dangerouslySetInnerHTML: { __html: tab.content } })
                         );
