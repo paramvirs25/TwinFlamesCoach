d9f05c594fee02ed9f20063769baa4df0b9e2537
Param <paramvirs25@gmail.com>
2023-07-28 23:02:45 +0530

    Gutenberg block shows h5, audio, dropbox  button

--- PHP/Tfc_Audio_Control/js/block.js
+++ PHP/Tfc_Audio_Control/js/block.js
@@ -4,6 +4,7 @@
     var el = wp.element.createElement;
     var InspectorControls = wp.blockEditor.InspectorControls;
     var TextControl = wp.components.TextControl;
+    var RawHTML = wp.element.RawHTML;
 
     registerBlockType('tfc-audio-control/block', {
         title: __('Tfc Audio Control', 'tfc-audio-control'),
@@ -25,7 +26,7 @@
         },
         edit: function (props) {
             var attributes = props.attributes;
-    
+
             return [
                 el(InspectorControls, {
                     key: 'inspector'
@@ -59,17 +60,20 @@
                     }
                 }))),
                 el('div', {
-                    className: 'audio-player'
-                }, el('audio', {
-                    controls: true,
-                    src: 'https://docs.google.com/uc?export=open&id=' + attributes.audio_file_id
-                })),
-                el('div', {
-                    className: 'audio-language-display'
-                }, attributes.audio_file_language) // Display audio_file_language in the editor
+                    className: 'contentbox'
+                },
+                    el('h5', null, attributes.audio_file_language), // H5 block displaying audio_file_language
+                    el('audio', {
+                        controls: true,
+                        src: 'https://docs.google.com/uc?export=open&id=' + attributes.audio_file_id
+                    }), // AUDIO Gutenberg block
+                    el(RawHTML, {
+                        key: 'dropbox-link',
+                        className: 'has-text-align-center'
+                    }, '<a class="dropbox-saver dropbox-dropin-btn dropbox-dropin-default" data-filename="' + attributes.audio_file_name + '" href="https://docs.google.com/uc?export=download&id=' + attributes.audio_file_id + '"><span class="dropin-btn-status"></span>Save to Dropbox</a>') // "Save to Dropbox" link
+                ),
             ];
         },
-       
         save: function () {
             return null; // Rendered on the front-end using PHP callback
         }
