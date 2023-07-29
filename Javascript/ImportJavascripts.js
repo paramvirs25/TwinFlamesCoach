e56209cbe505eeeedf992c23e5530e78de9a9716
Param <paramvirs25@gmail.com>
2023-07-29 17:44:52 +0530

    removed checkAndImportDropboxScript

--- Javascript/ImportJavascripts.js
+++ Javascript/ImportJavascripts.js
@@ -1,21 +1,17 @@
 class TfcImportJavascripts {
     static importDropboxScript() {
         if (!TfcImportJavascripts.dropboxScriptLoaded) {
-            var script = document.createElement('script');
-            script.src = 'https://www.dropbox.com/static/api/2/dropins.js';
-            script.id = 'dropboxjs';
-            script.setAttribute('data-app-key', 'z2xhg87mpzhvs3n');
-            script.defer = true;
-            document.head.appendChild(script);
-            console.log("Script Loaded - Dropbox - dropins.js");
-            TfcImportJavascripts.dropboxScriptLoaded = true;
-        }
-    }
-
-    static checkAndImportDropboxScript() {
-        var dropboxSaverLinks = document.querySelectorAll('a.dropbox-saver');
-        if (dropboxSaverLinks.length > 0) {
-            TfcImportJavascripts.importDropboxScript();
+            // Check if there are any dropbox-saver links on the page
+            var dropboxSaverLinks = document.querySelectorAll('a.dropbox-saver');
+            if (dropboxSaverLinks.length > 0) {
+                var script = document.createElement('script');
+                script.src = 'https://www.dropbox.com/static/api/2/dropins.js';
+                script.id = 'dropboxjs';
+                script.setAttribute('data-app-key', 'z2xhg87mpzhvs3n');
+                script.defer = true;
+                document.head.appendChild(script);
+                TfcImportJavascripts.dropboxScriptLoaded = true;
+            }
         }
     }
 }
@@ -26,7 +22,7 @@ TfcImportJavascripts.dropboxScriptLoaded = false;
 window.addEventListener('load', function () {
     // Call the method on page load (frontend only)
     if (typeof wp.element === 'undefined') {
-        TfcImportJavascripts.checkAndImportDropboxScript();
+        TfcImportJavascripts.importDropboxScript();
     }
 });
 
