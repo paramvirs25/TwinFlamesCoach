0d6aaa38ebf6c1419a8492e6025befaab767c58e
Param <paramvirs25@gmail.com>
2023-07-30 09:49:57 +0530

    buttons, horixzontal scroll, visuallink css

--- Javascript/ImportJavascripts.js
+++ Javascript/ImportJavascripts.js
@@ -57,18 +57,60 @@ class TfcImportJavascripts {
       console.log("importKiokenTabsFixScriptCss");
     }
   }
+
+  static importButtonsCss() {
+    // Check if there are any anchor tags with both classes 'wp-block-button__link' and 'wp-element-button'
+    var buttonsLinks = document.querySelectorAll('a.wp-block-button__link.wp-element-button');
+    if (buttonsLinks.length > 0) {
+      this.loadCSS('https://paramvirs25.github.io/TwinFlamesCoach/Css/Buttons.css');
+
+      TfcImportJavascripts.buttonCssLoaded = true;
+      console.log("importButtonsCss");
+    }
+  }
+
+  static importHorizontalScrollCss() {
+    // Check if there are any elements with the class 'tfcScrollHorizRow' on the page
+    var horScrlItems = document.querySelectorAll('.tfcScrollHorizRow');
+    if (horScrlItems.length > 0) {
+      this.loadCSS('https://paramvirs25.github.io/TwinFlamesCoach/Css/HorizontalScroll.css');
+
+      TfcImportJavascripts.horizontalScrollCssLoaded = true;
+      console.log("importHorizontalScrollCss");
+    }
+  }
+
+  static importVisualLinkPreviewCss() {
+    // Check if there are any elements with the class 'vlp-link-summary' on the page
+    var visualLinkItems = document.querySelectorAll('.vlp-link-summary');
+    if (visualLinkItems.length > 0) {
+      this.loadCSS('https://paramvirs25.github.io/TwinFlamesCoach/Css/VisualLinkPreview.css');
+
+      TfcImportJavascripts.visualLinkPreviewCssLoaded = true;
+      console.log("importVisualLinkPreviewCss");
+    }
+  }
+
 }
 
 // Set the initial values
 TfcImportJavascripts.dropboxScriptLoaded = false;
 TfcImportJavascripts.kiokenAccordionFixScriptLoaded = false;
 TfcImportJavascripts.kiokenTabsFixScriptLoaded = false;
+TfcImportJavascripts.buttonCssLoaded = false;
+TfcImportJavascripts.horizontalScrollCssLoaded = false;
+TfcImportJavascripts.visualLinkPreviewCssLoaded = false;
 
 window.addEventListener('load', function () {
   // Call the methods on page load (frontend only)
   if (typeof wp.element === 'undefined') {
     TfcImportJavascripts.importDropboxScript();
+
     TfcImportJavascripts.importKiokenAccordionFixScriptCss();
     TfcImportJavascripts.importKiokenTabsFixScriptCss();
+
+    TfcImportJavascripts.importButtonsCss();
+    TfcImportJavascripts.importHorizontalScrollCss();
+    TfcImportJavascripts.importVisualLinkPreviewCss();
   }
 });
