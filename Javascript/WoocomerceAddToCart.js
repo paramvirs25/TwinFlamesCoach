6eb3bbba5814c38a3aaca79c3d159c2af82b8fa6
Param <paramvirs25@gmail.com>
2023-09-24 22:22:02 +0530

    button color

--- Javascript/WoocomerceAddToCart.js
+++ Javascript/WoocomerceAddToCart.js
@@ -12,14 +12,17 @@ jQuery(document).ready(function($) {
 			modal: true,
 			title: "Confirmation",
 			buttons: {
-				"PROCEED": function() {
-					// Perform the "Add to Cart" action here
-					// You can put your WooCommerce code here
-					//alert("Item added to cart!");
-					$(this).dialog("close");
+				"PROCEED": {
+                    text: "PROCEED",
+                    class: "confirmButton",
+                    click: function() {
+                        // Perform the "Add to Cart" action here
+                        // You can put your WooCommerce code here
+                        $(this).dialog("close");
 					
-					// Now, you can trigger the form submission
-              		$(".single_add_to_cart_button").unbind('click').click();
+                        // Now, you can trigger the form submission
+                        $(".single_add_to_cart_button").unbind('click').click();
+                    }
 				},
 				"CANCEL": function() {
 					// Close the magical window without any action
