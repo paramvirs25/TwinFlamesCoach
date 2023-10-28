cec18d566676bac20853567d69e82c5aad3156c5
Param <paramvirs25@gmail.com>
2023-10-28 14:37:41 +0530

    calculatePriceWithDiscount calculation

--- ShortCodes/CountryPriceCalculation.php
+++ ShortCodes/CountryPriceCalculation.php
@@ -1,4 +1,81 @@
 <?php
+
+/**
+ * Calculate and display the price with optional discounts based on the user's location.
+ *
+ * @param array $atts
+ * @return string HTML list of items
+ */
+function calculatePriceWithDiscount( $atts ) {
+	$inrPrice = $atts['inr'];
+	$discounts = $atts['discount'];
+	$isShowDiscount = false;
+	$currencySymbol = "INR ₹";
+	$currencyMultiplier = 1;
+	$output = "";
+
+	if (is_null($inrPrice) || $inrPrice === '') {
+		return $output;
+	}
+
+	// If the geotarget API exists
+	if (function_exists('geot_target')) {
+
+		// Calculate the final price by applying discounts, if any
+		if (!is_null($discounts) && $discounts !== '') {
+			$isShowDiscount = true;
+
+			// Extract discounts from the 'discount' attribute
+			$discounts = explode(',', $discounts);
+			$discountValues = array();
+			$classCounts = array();
+
+			foreach ($discounts as $discount) {
+				$discountPair = explode('=', $discount);
+				if (count($discountPair) === 2) {
+					$key = $discountPair[0];
+					$value = $discountPair[1];
+					if (strpos($key, 'classes') === 0) {
+						$classCounts[] = $value;
+					} elseif (strpos($key, 'discount') === 0) {
+						$discountValues[] = $value;
+					}
+				}
+			}			
+		}
+
+		// Format the price based on the user's country
+		if (!geot_target('IN')) {
+			// For the rest of the world in USD
+			$currencySymbol = 'USD $';
+			$currencyMultiplier = 0.02;
+		}
+
+		$beforeDiscountPriceFormated = number_format($inrPrice * $currencyMultiplier, 2);
+
+		// Generate the HTML list of items
+		$output = "<ul>";
+		$output .= "<li>The cost for a single class is: $currencySymbol $beforeDiscountPriceFormated</li>";
+
+		if ($isShowDiscount) {
+			foreach ($classCounts as $index => $classCount) {
+				$discountValue = $discountValues[$index];
+				$totalCost = $inrPrice * $classCount;
+				$discountAmount = $totalCost - ($totalCost * $discountValue / 100);
+				$discountedPriceFormatted = number_format($discountAmount * $currencyMultiplier, 2);
+
+				$output .= "<li>You can save $discountValue% when you sign up for $classCount classes. The total cost after the discount is: <br/><span><s>$currencySymbol $totalCost</s></span> <span style='color:#77a464;'>$currencySymbol $discountedPriceFormatted</span></li>";
+			}
+		}
+
+		$output .= "</ul>";
+	}
+
+	return $output;
+}
+add_shortcode('country_price_discount', 'calculatePriceWithDiscount');
+
+
 /**
  * Applies discount,
  *      currency symbol,
@@ -17,7 +94,6 @@
 function calculatePrice( $atts ) {
 	$inrPrice = $atts['inr'];
     $discount = $atts['discount'];
-    $discountedPrice = 	$inrPrice;
     $isShowDiscount = false;    
     $currencySymbol = "INR ₹";
     $currencyMultiplier = 1;
@@ -59,4 +135,3 @@ function calculatePrice( $atts ) {
     return $out;
 }
 add_shortcode( 'country_price', 'calculatePrice' );
-?>
