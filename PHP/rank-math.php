08ecb198fed26e393ff8a4ed9995ede1c6e4e2e2
Param <paramvirs25@gmail.com>
2023-12-18 20:14:51 +0530

    Rank math snippet for ‘hasMerchantReturnPolicy’

--- PHP/rank-math.php
+++ PHP/rank-math.php
@@ -1,10 +1,20 @@
 <?php
 
 /**
+ * For twinflamescoach.com
  * Change the Focus Keyword Limit
  */
 add_filter( 'rank_math/focus_keyword/maxtags', function() {
     return 10; // Number of Focus Keywords. 
+});
+
+/**
+ * For Members.twinflamescoach.com
+ * Missing field ‘hasMerchantReturnPolicy’ (in ‘offers’)
+ */
+add_filter( 'rank_math/snippet/rich_snippet_product_entity', function( $entity ) {
+    $entity['offers']['hasMerchantReturnPolicy'] = false;
+    return $entity;
 });
 
 ?>
