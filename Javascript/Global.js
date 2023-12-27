cd464fe8d2058725ff41f49369ce10f02f20834b
Param <paramvirs25@gmail.com>
2023-12-27 19:49:50 +0530

    New loadFile method

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -6,28 +6,9 @@ class TfcGlobal {
         return `${TfcGlobal.CssJsPath}/${fileUrl}`;
     }
 
-    // static loadFile(divToCreateId, filePath) {
-    //     const dynamicDiv = document.createElement('div');
-    //     dynamicDiv.id = divToCreateId; // Set the id of the div 
-
-    //     // Find the current script tag
-    //     const currentScriptTag = document.currentScript;
-
-    //     if (currentScriptTag) {
-    //         // Insert the dynamically created div after the current script tag
-    //         currentScriptTag.parentNode.insertBefore(dynamicDiv, currentScriptTag.nextSibling);
-    //     } else {
-    //         // Fallback: if currentScript is not supported, use querySelector
-    //         const scriptTags = document.querySelectorAll('script[src*=TfcGlobal]');
-    //         if (scriptTags.length > 0) {
-    //             scriptTags[scriptTags.length - 1].parentNode.insertBefore(dynamicDiv, null);
-    //         }
-    //     }
-
-    //     // Load content into the dynamically created div
-    //     var fullFileUrl = TfcGlobal.getFullFileUrl(filePath);
-    //     jQuery(dynamicDiv).load(fullFileUrl);
-
-    //     console.log("Loaded file " + fullFileUrl);
-    // }
+    static loadFile(divToLoadHtml, htmlFilePath) {
+        var fullFileUrl = TfcGlobal.getFullFileUrl(htmlFilePath);
+        jQuery(`#${divToLoadHtml}`).load(fullFileUrl);
+        console.log("Loaded HTML file " + fullFileUrl);
+    }
 }
