3f7c914be11d20754f93bae57dc25065e8e7cc16
Param <paramvirs25@gmail.com>
2024-01-03 19:59:15 +0530

    Auto discount on repeating BIW1

--- WooCommerce/PHP/WooCommerce_apply_auto_coupon_discounts.php
+++ WooCommerce/PHP/WooCommerce_apply_auto_coupon_discounts.php
@@ -17,6 +17,8 @@ function apply_auto_coupon_discounts( $cart ){
 	}
 	
 	apply_course_completion_discount_via_coupon_on_consultation( $cart );
+
+	apply_basicIW1_repeater_discount( $cart );
 	
 }
 
@@ -25,7 +27,7 @@ function apply_course_completion_discount_via_coupon_on_consultation( $cart ) {
     $product_id = 11782; // Set product ID for CONSULTATION product
 
 	// Check if cart contains CONSULTATION product
-	if ( ! is_cart_contains_consultation_product( $cart, $product_id ) ) {
+	if ( ! is_cart_contains_product( $cart, $product_id ) ) {
 		return; // Terminate function if cart does not contain CONSULTATION product
 	}
 
@@ -52,15 +54,47 @@ function apply_course_completion_discount_via_coupon_on_consultation( $cart ) {
     }
 }
 
-function is_cart_contains_consultation_product( $cart, $product_id ){
-	$cart_contains_consultation = false;
+function apply_basicIW1_repeater_discount( $cart ) {
+	
+    $product_id = 11726; // Set product ID BAsic IW 1 product
+
+	// Check if cart contains this product
+	if ( ! is_cart_contains_product( $cart, $product_id ) ) {
+		return; // Terminate function if cart does not contain this product
+	}
+
+    // Get logged-in user's roles
+    $user = wp_get_current_user();
+    $user_roles = $user->roles;
+
+    // Get all courses and check if user has a matching role
+    $course = TFCMembers\Courses::getBasicIw1();
+	
+	//foreach ($courses as $course) {
+		if (in_array($course->courseRoleName, $user_roles)) {
+			//echo "<p>Course " . $course->courseName . "</p>";
+			
+            // User has this course's role, so apply discount via coupon code
+            $coupon_code = "repeaterbiw1";
+			//$course->courseCompletionCouponForDiscount; // Set coupon code here
+            $coupon = new WC_Coupon($coupon_code);
+			//$coupon->set_remove_message(''); //dont show any mesage when coupon is removed from cart.
+            if ($coupon->is_valid()) { // check if coupon is valid				
+                $cart->add_discount($coupon_code);
+				wc_clear_notices(); // Clear coupon code applied notice				
+            }
+        }
+    //}
+}
+
+function is_cart_contains_product( $cart, $product_id ){
+	$cart_contains_product = false;
 	foreach ( $cart->get_cart_contents() as $item ) {
 		if ( $item['product_id'] == $product_id ) {
-			$cart_contains_consultation = true;
+			$cart_contains_product = true;
 			break;
 		}
 	}
 	
-	return $cart_contains_consultation;
+	return $cart_contains_product;
 }
-?>
