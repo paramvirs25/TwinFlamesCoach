9d34d9dc1c9acb76d1f5138f3e8c47fd7fcbd8bc
Param <paramvirs25@gmail.com>
2024-01-12 22:19:25 +0530

    Gauge Chart working

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -1,6 +1,7 @@
 class TfcGlobal {
     static CssJsPath = "https://paramvirs25.github.io/TwinFlamesCoach"; // URL path
     static ShuffleToolsListPath = "HTML/Shuffle/ShuffleToolsList.html";
+    static GaugeChartPath = "gauge-chart/index.html";
 
     static getFullFileUrl(fileUrl) {
         return `${TfcGlobal.CssJsPath}/${fileUrl}`;
