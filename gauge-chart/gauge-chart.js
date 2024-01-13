6c89e71477f48c3e58180419aa26f4743805cb47
Param <paramvirs25@gmail.com>
2024-01-13 15:57:06 +0530

    Loding dx.all from inside of script

--- gauge-chart/gauge-chart.js
+++ gauge-chart/gauge-chart.js
@@ -15,24 +15,30 @@ jQuery(function ($) {
       return {
         value: this._initialValue,
         valueIndicator: {
-          color: '#fff' },
+          color: '#fff'
+        },
 
         geometry: {
           startAngle: 180,
-          endAngle: 360 },
+          endAngle: 360
+        },
 
         scale: {
           startValue: 0,
           endValue: this._higherValue,
           customTicks: [0, 33, 67, 100],
           tick: {
-            length: 4 },
+            length: 4
+          },
 
           label: {
             font: {
               color: '#87959f',
               size: 19,
-              family: '"Open Sans", sans-serif' } } },
+              family: '"Open Sans", sans-serif'
+            }
+          }
+        },
 
 
 
@@ -42,7 +48,8 @@ jQuery(function ($) {
           font: {
             family: '"Open Sans", sans-serif',
             color: '#fff',
-            size: 10 },
+            size: 10
+          },
 
           subtitle: {
             text: this._subtitle,
@@ -50,7 +57,10 @@ jQuery(function ($) {
               family: '"Open Sans", sans-serif',
               color: '#fff',
               weight: 700,
-              size: 28 } } },
+              size: 28
+            }
+          }
+        },
 
 
 
@@ -62,53 +72,58 @@ jQuery(function ($) {
           currentGauge.find('.dxg-title text').first().attr('y', 48);
           currentGauge.find('.dxg-title text').last().attr('y', 28);
           currentGauge.find('.dxg-value-indicator').append(border, circle);
-        } };
+        }
+      };
 
 
     }
 
     init() {
       jQuery(this._element).dxCircularGauge(this._buildConfig());
-    }}
+    }
+  }
 
 
   jQuery(document).ready(function ($) {
-    let higherValue = 100;
-
-    $('.gauge').each(function (index, item) {
-      let params = {
-        initialValue: 0,
-        higherValue: higherValue,
-        title: `Score`,
-        subtitle: '0' };
-
-
-      let gauge = new GaugeChart(item, params);
-      gauge.init();
-    });
-
-    $('#random').click(function () {
+    $.getScript("https://paramvirs25.github.io/TwinFlamesCoach/gauge-chart/dx.all.js", function () {
+      let higherValue = 100;
 
       $('.gauge').each(function (index, item) {
-        let gauge = $(item).dxCircularGauge('instance');
-        let randomNum = Math.round(Math.random() * higherValue);
-        let gaugeElement = $(gauge._$element[0]);
+        let params = {
+          initialValue: 0,
+          higherValue: higherValue,
+          title: `Score`,
+          subtitle: '0'
+        };
 
-        let answer = "";
-        if(randomNum <= 33 ){
-          answer = `Consider it NO.`;
-        } else if(randomNum > 33 && randomNum <= 66){ //33.33 to 66.66
-          answer = `You may take it as YES, although recommended is NO.`;
-        } else if(randomNum > 66 && randomNum <= 85){ //33.33 to 66.66
-          answer = `You can take it as YES!`;
-        } else if(randomNum > 85){ //33.33 to 66.66
-          answer = `Absolutely YES!`;
-        }
 
-        gaugeElement.find('.dxg-title text').last().html(`${randomNum}`);
-        gauge.value(randomNum);
+        let gauge = new GaugeChart(item, params);
+        gauge.init();
+      });
 
-        $('#interpretation').html(answer);
+      $('#random').click(function () {
+
+        $('.gauge').each(function (index, item) {
+          let gauge = $(item).dxCircularGauge('instance');
+          let randomNum = Math.round(Math.random() * higherValue);
+          let gaugeElement = $(gauge._$element[0]);
+
+          let answer = "";
+          if (randomNum <= 33) {
+            answer = `Consider it NO.`;
+          } else if (randomNum > 33 && randomNum <= 66) { //33.33 to 66.66
+            answer = `You may take it as YES, although recommended is NO.`;
+          } else if (randomNum > 66 && randomNum <= 85) { //33.33 to 66.66
+            answer = `You can take it as YES!`;
+          } else if (randomNum > 85) { //33.33 to 66.66
+            answer = `Absolutely YES!`;
+          }
+
+          gaugeElement.find('.dxg-title text').last().html(`${randomNum}`);
+          gauge.value(randomNum);
+
+          $('#interpretation').html(answer);
+        });
       });
     });
   });
