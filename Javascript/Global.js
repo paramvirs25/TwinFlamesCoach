6d37b1c7cb5649b4e9184efc34b5eb154368af0e
Param <paramvirs25@gmail.com>
2024-06-03 16:43:35 +0530

    Emotions chart tool

--- Javascript/Global.js
+++ Javascript/Global.js
@@ -2,6 +2,7 @@ class TfcGlobal {
     static CssJsPath = "https://paramvirs25.github.io/TwinFlamesCoach"; // URL path
     static ShuffleToolsListPath = "HTML/Shuffle/ShuffleToolsList.html";
     static GaugeChartPath = "gauge-chart/index.html";
+    static EmotionsChartPath = "Tools/emotions-chart/index.html";
 
     static getFullFileUrl(fileUrl) {
         return `${TfcGlobal.CssJsPath}/${fileUrl}`;
