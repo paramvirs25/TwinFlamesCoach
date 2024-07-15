f80c8912568a28b9c62a65be26acb798ca070424
Param <paramvirs25@gmail.com>
2024-07-15 20:05:30 +0530

    printed var tfcIWProgressPercentage in script

--- ShortCodes/InnerWorkProgressGauge.php
+++ ShortCodes/InnerWorkProgressGauge.php
@@ -1,30 +1,35 @@
 <?php
 namespace TFCMembers;
 
-add_shortcode( 'tfc_inner_work_progress', function () {
+add_shortcode('tfc_inner_work_progress', function () {
     $current_user = wp_get_current_user(); // Get the current logged-in user object
-	$user_roles = $current_user->roles; // Get an array of the user's assigned roles
-	
-	//return "";
+    $user_roles = $current_user->roles; // Get an array of the user's assigned roles
+    
+    //return "";
 
-	return \TFCMembers\TwinFlamesCoachInnerWorkProgress::courseCompletionStatus($user_roles);
-} );
+    return \TFCMembers\TwinFlamesCoachInnerWorkProgress::courseCompletionStatus($user_roles);
+});
 
 class TwinFlamesCoachInnerWorkProgress {
     
     public static function courseCompletionStatus($userRoles) {
+        $totalIWProgress = 0;
         $courses = \TFCMembers\Courses::getCoursesListForTFCIWProgress();
         $html = '';
 
         $html .= "<div id='tfcIWProgress'>";
         foreach ($courses as $course) {
-            
-            $icon = in_array($course->courseRoleName, $userRoles) ? 
-                '<span style="color:green">&#10003</span>' : '<span style="color:blue">&#10060</span>';
+            if (in_array($course->courseRoleName, $userRoles)) {
+                $icon = '<span style="color:green">&#10003</span>';
+                $totalIWProgress += $course->tfcIWProgressPercentage; // Using += for readability
+            } else {
+                $icon = '<span style="color:blue">&#10060</span>';
+            }
             
             $html .= "<p>{$icon} <span class='tfcIWProgPerc'>({$course->tfcIWProgressPercentage}%)</span> {$course->courseName}</p>";
         }
         $html .= "</div>";
+        $html .= "<script>var tfcIWProgressPercentage = {$totalIWProgress};</script>"; // Use $totalIWProgress
 
         return $html;
     }
