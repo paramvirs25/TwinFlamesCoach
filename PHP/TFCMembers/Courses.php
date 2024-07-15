75800d2b03c04bc94ef6a6cfe74207bd0744a3dc
Param <paramvirs25@gmail.com>
2024-07-15 19:54:43 +0530

    Courses class update

--- PHP/TFCMembers/Courses.php
+++ PHP/TFCMembers/Courses.php
@@ -1,7 +1,7 @@
 <?php
 namespace TFCMembers;
 
-/*This class list all our courses and respective course details like, course completion role name, course completion discount*/
+/*This class lists all our courses and respective course details like course completion role name and course completion discount*/
 class Courses {
     const BASIC_IW_1 = "basic_iw_1";
     const TFCIW = "tfciw";
@@ -15,138 +15,79 @@ class Courses {
     const APPRENTICE_COACH = "apprentice_coach";
     const KRIYA_KUNDALINI_TEACHER = "kriya_kundalini_teacher"; // this course is not conducted by us. Only use it in inner work progress meter
 
-    private static $basic_iw_1 = null;
-    public static function getBasicIw1()
-    {
-        return self::$basic_iw_1 ?? self::$basic_iw_1 = 
-            new Courses("Basic inner work 1", self::BASIC_IW_1, "Basic IW 1", 14);
-    }
+    private static $courses = [
+        self::BASIC_IW_1 => ["Basic inner work 1", self::BASIC_IW_1, "Basic IW 1", 14],
+        self::TFCIW => ["Basic inner work 2", self::TFCIW, "Basic IW 2", 24],
+        self::TFCAIW1 => ["Advanced inner work 1", self::TFCAIW1, "Advanced IW 1", 0],
+        self::ADV_TF_HEALINGS_1 => ["Advanced Twin Flame Healings 1", self::ADV_TF_HEALINGS_1, "Advanced TF Healings 1", 0],
+        self::CHAKRA_HEALING_BALANCING => ["Chakra balancing program", self::CHAKRA_HEALING_BALANCING, "Chakra balancing 1", 0],
+        self::CERTIFIED_COACH => ["Twin Flames Coach program", self::CERTIFIED_COACH, "Twin Flames Coach 1", 54],
+        self::CERTIFIED_YOGA_TEACHER => ["Yoga Teacher Training program", self::CERTIFIED_YOGA_TEACHER, "Yoga Teacher Training", 0],
+        self::SHAKTI_KAWACH => ["Shakti Kavach program", self::SHAKTI_KAWACH, "Shakti Kavach", 0],
+        self::APPRENTICE_BASIC_IW => ["Apprentice - Basic IW", self::APPRENTICE_BASIC_IW, "Apprenticeship Basic IW", 8],
+        self::APPRENTICE_COACH => ["Apprentice - Twin Flames Coach", self::APPRENTICE_COACH, "Apprenticeship TF Coach", 0],
+        self::KRIYA_KUNDALINI_TEACHER => ["Kriya Kundalini Teacher", self::KRIYA_KUNDALINI_TEACHER, "Kriya Kundalini Teacher", 0],
+    ];
 
-    private static $tfciw = null;
-    public static function getTfciw()
-    {
-        return self::$tfciw ?? self::$tfciw = 
-            new Courses("Basic inner work 2", self::TFCIW, "Basic IW 2", 24);
-    }
+    private static $instances = [];
 
-    private static $tfcaiw1 = null;
-    public static function getTfcaiw1()
-    {
-        return self::$tfcaiw1 ?? self::$tfcaiw1 = 
-            new Courses("Advanced inner work 1", self::TFCAIW1, "Advanced IW 1", 0);
-    }
-
-    private static $advanced_tf_healings_1 = null;
-    public static function getAdvancedTfHealings1()
-    {
-        return self::$advanced_tf_healings_1 ?? self::$advanced_tf_healings_1 = 
-            new Courses("Advanced Twin Flame Healings 1", self::ADV_TF_HEALINGS_1, "Advanced TF Healings 1", 0);
-    }
-
-    private static $chakra_healing_balancing = null;
-    public static function getChakraHealingBalancing()
-    {
-        return self::$chakra_healing_balancing ?? self::$chakra_healing_balancing = 
-            new Courses("Chakra balancing program", self::CHAKRA_HEALING_BALANCING, "Chakra balancing 1", 0);
-    }
-
-    private static $certified_coach = null;
-    public static function getCertifiedCoach()
-    {
-        return self::$certified_coach ?? self::$certified_coach = 
-            new Courses("Twin Flames Coach program", self::CERTIFIED_COACH, "Twin Flames Coach 1", 54);
-    }
-
-    private static $certified_yoga_teacher = null;
-    public static function getCertifiedYogaTeacher()
-    {
-        return self::$certified_yoga_teacher ?? self::$certified_yoga_teacher = 
-            new Courses("Yoga Teacher Training program", self::CERTIFIED_YOGA_TEACHER, "Yoga Teacher Training", 0);
-    }
-
-    private static $shakti_kawach = null;
-    public static function getShaktiKawach()
-    {
-        return self::$shakti_kawach ?? self::$shakti_kawach = 
-            new Courses("Shakti Kavach program", self::SHAKTI_KAWACH, "Shakti Kavach", 0);
-    }
-
-    private static $apprentice_basic_iw = null;
-    public static function getApprenticeBasicIw()
-    {
-        return self::$apprentice_basic_iw ?? self::$apprentice_basic_iw = 
-            new Courses("Apprentice - Basic IW", self::APPRENTICE_BASIC_IW, "Apprenticeship Basic IW", 8);
-    }
-
-    private static $apprentice_coach = null;
-    public static function getApprenticeCoach()
-    {
-        return self::$apprentice_coach ?? self::$apprentice_coach = 
-            new Courses("Apprentice - Twin Flames Coach", self::APPRENTICE_COACH, "Apprenticeship TF Coach", 0);
-    }
-
-    private static $kriya_kundalini_teacher = null;
-    public static function getKriyaKundaliniTeacher()
-    {
-        return self::$kriya_kundalini_teacher ?? self::$kriya_kundalini_teacher = 
-            new Courses("Kriya Kundalini Teacher", self::KRIYA_KUNDALINI_TEACHER, "kriya kundalini teacher", 0);
-    }
-    
     public $courseName;
     public $courseRoleName;
     public $courseCompletionCouponForDiscount; //course completion discount can be applied from a coupon mentioned here
     public $tfcIWProgressPercentage;
-    
-    public function __construct($name, $roleName, $completionCouponForDiscount, $tfcIWProgressPercentage) {
+
+    private function __construct($name, $roleName, $completionCouponForDiscount, $tfcIWProgressPercentage) {
         $this->courseName = $name;
         $this->courseRoleName = $roleName;
         $this->courseCompletionCouponForDiscount = $completionCouponForDiscount;
         $this->tfcIWProgressPercentage = $tfcIWProgressPercentage;
     }
-    
-    /**
-     * This method is being used in
-     * 1 WooCommerce_apply_auto_coupon_discounts
-     * 2 
-     */
+
+    public static function getCourse($key) {
+        if (!isset(self::$courses[$key])) {
+            throw new \Exception("Invalid course key: $key");
+        }
+
+        if (!isset(self::$instances[$key])) {
+            [$name, $roleName, $completionCouponForDiscount, $tfcIWProgressPercentage] = self::$courses[$key];
+            self::$instances[$key] = new self($name, $roleName, $completionCouponForDiscount, $tfcIWProgressPercentage);
+        }
+
+        return self::$instances[$key];
+    }
+
     public static function getAllCourses() {
-        $courses = array(
-            self::BASIC_IW_1 => self::getBasicIw1(),
-            self::TFCIW => self::getTfciw(),
-            self::TFCAIW1 => self::getTfcaiw1(),
-            self::ADV_TF_HEALINGS_1 => self::getAdvancedTfHealings1(),
-            self::CHAKRA_HEALING_BALANCING => self::getChakraHealingBalancing(),
-            self::CERTIFIED_COACH => self::getCertifiedCoach(),
-            self::CERTIFIED_YOGA_TEACHER => self::getCertifiedYogaTeacher(),
-            self::SHAKTI_KAWACH => self::getShaktiKawach(),
-            self::APPRENTICE_BASIC_IW => self::getApprenticeBasicIw(),
-            self::APPRENTICE_COACH => self::getApprenticeCoach(),
-        );
-        return $courses;
-    }    
+        return array_map(function($key) {
+            return self::getCourse($key);
+        }, array_keys(self::$courses));
+    }
 
     public static function getCoursesListForSupportCoaches() {
-        $courses = array(
-            self::BASIC_IW_1 => self::getBasicIw1(),
-            self::TFCIW => self::getTfciw(),
-            self::ADV_TF_HEALINGS_1 => self::getAdvancedTfHealings1(),
-            self::CERTIFIED_COACH => self::getCertifiedCoach(),
-            self::APPRENTICE_BASIC_IW => self::getApprenticeBasicIw(),            
-        );
-        return $courses;
+        $keys = [
+            self::BASIC_IW_1,
+            self::TFCIW,
+            self::ADV_TF_HEALINGS_1,
+            self::CERTIFIED_COACH,
+            self::APPRENTICE_BASIC_IW,
+        ];
+
+        return self::getCoursesByKeys($keys);
     }
 
     public static function getCoursesListForTFCIWProgress() {
-        $courses = array(
-            self::BASIC_IW_1 => self::getBasicIw1(),
-            self::TFCIW => self::getTfciw(),
-            //self::ADV_TF_HEALINGS_1 => self::getAdvancedTfHealings1(),    //score-26
-            //self::CHAKRA_HEALING_BALANCING => self::getChakraHealingBalancing(), //score-38
-            self::CERTIFIED_COACH => self::getCertifiedCoach(),
-            self::APPRENTICE_BASIC_IW => self::getApprenticeBasicIw(),
-            //self::APPRENTICE_COACH => self::getApprenticeCoach(),
-            //self::KRIYA_KUNDALINI_TEACHER => self::getKriyaKundaliniTeacher(),
-        );
-        return $courses;
+        $keys = [
+            self::BASIC_IW_1,
+            self::TFCIW,
+            self::CERTIFIED_COACH,
+            self::APPRENTICE_BASIC_IW,
+        ];
+
+        return self::getCoursesByKeys($keys);
+    }
+
+    private static function getCoursesByKeys(array $keys) {
+        return array_map(function($key) {
+            return self::getCourse($key);
+        }, $keys);
     }
 }
