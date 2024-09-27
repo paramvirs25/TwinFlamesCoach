c579874d66cdef1627aaa365a6f45f74fb0965b9
Param <paramvirs25@gmail.com>
2024-09-27 21:18:22 +0530

    auto click not working

--- ShortCodes/WhatsAppButtons.php
+++ ShortCodes/WhatsAppButtons.php
@@ -1,19 +1,23 @@
 <?php
-function whatsapp_button($phone_number, $message){
-    // Sanitize message
-    $message = urlencode($message);
-    
-    // WhatsApp URL
-    $whatsapp_url = "https://api.whatsapp.com/send?phone=$phone_number&text=$message";
+function create_whatsapp_button($isAutoClick, $url, $text){
+    // Check if auto-click class should be added
+    $autoClickLinkText = ($isAutoClick == "true") ? " auto-click-link" : ""; 
 
     // Output WhatsApp button HTML
     $output = '<div class="wp-block-jetpack-send-a-message">';
     $output .= '<div class="wp-block-jetpack-whatsapp-button aligncenter is-color-dark">';
-    $output .= '<a class="whatsapp-block__button" href="' . $whatsapp_url . '" style="background-color:#25D366;color:#fff" target="_blank" rel="noopener noreferrer">WhatsApp to Program Support</a>';
+    $output .= '<a class="whatsapp-block__button' . $autoClickLinkText . '" href="' . $url . '" style="background-color:#25D366;color:#fff" target="_blank" rel="noopener noreferrer">' . $text . '</a>';
     $output .= '</div>';
     $output .= '</div>';
 
     return $output;
+}
+
+function whatsapp_url($phone_number, $message){
+    // Sanitize message
+    $message = urlencode($message);
+
+    return "https://api.whatsapp.com/send?phone=$phone_number&text=$message";
 }
 
 function whatsapp_program_support_shortcode($atts, $content = null) {
@@ -25,7 +29,24 @@ function whatsapp_program_support_shortcode($atts, $content = null) {
     // Hardcoded phone number
     $phone_number = '919417302025';
 
-    return whatsapp_button($phone_number, $atts['message']);
+    return create_whatsapp_button(
+        "false", 
+        whatsapp_url($phone_number, $atts['message']),
+        "WhatsApp to Program Support"
+    );
 }
 
+function whatsapp_group_button_shortcode($atts, $content = null) {
+    // Extract attributes
+    $atts = shortcode_atts(array(
+        'isAutoClick' => '',
+        'url' => '',
+        'text' => ''
+    ), $atts);
+
+    return create_whatsapp_button($atts['isAutoClick'], $atts['url'], $atts['text']);
+}
+
+// Register the shortcodes
 add_shortcode('whatsapp_program_support', 'whatsapp_program_support_shortcode');
+add_shortcode('whatsapp_group_button', 'whatsapp_group_button_shortcode');
