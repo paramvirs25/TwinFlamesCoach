660b1024e60fd8ca10b47d2774b844e305111f1f
Param <paramvirs25@gmail.com>
2025-03-31 18:38:31 +0530

    confirm-click

--- ShortCodes/WhatsAppButtons.php
+++ ShortCodes/WhatsAppButtons.php
@@ -1,12 +1,13 @@
 <?php
-function create_whatsapp_button($isAutoClick, $url, $text){
+function create_whatsapp_button($isAutoClick, $url, $text, $isConfirmClick){
     // Ensure correct class is added for auto-click
     $autoClickLinkText = ($isAutoClick) ? " auto-click-link" : ""; 
+    $confirmClickText =  ($isConfirmClick) ? " confirm-click" : ""; 
 
     // Output WhatsApp button HTML
     $output = '<div class="wp-block-jetpack-send-a-message">';
-    $output .= '<div class="wp-block-jetpack-whatsapp-button aligncenter is-color-dark">';
-    $output .= '<a class="confirmFreeConsultModal whatsapp-block__button' . $autoClickLinkText . '" href="' . $url . '" style="background-color:#25D366;color:#fff" target="_self" rel="noopener noreferrer">' . $text . '</a>';
+    $output .= '<div class="wp-block-jetpack-whatsapp-button aligncenter is-color-dark' . $confirmClickText . '">';
+    $output .= '<a class="whatsapp-block__button' . $autoClickLinkText . '" href="' . $url . '" style="background-color:#25D366;color:#fff" target="_self" rel="noopener noreferrer">' . $text . '</a>';
     $output .= '</div>';
     $output .= '</div>';
 
@@ -18,22 +19,6 @@ function whatsapp_url($phone_number, $message){
     $message = urlencode($message);
 
     return "https://api.whatsapp.com/send?phone=$phone_number&text=$message";
-}
-
-function whatsapp_param_shortcode($atts, $content = null) {
-    // Extract attributes
-    $atts = shortcode_atts(array(
-        'message' => ''
-    ), $atts);
-
-    // Hardcoded phone number
-    $phone_number = '919478016938';
-
-    return create_whatsapp_button(
-        false,  // Now passing boolean
-        whatsapp_url($phone_number, $atts['message']),
-        "WhatsApp Jnana Param"
-    );
 }
 
 function whatsapp_rituom_shortcode($atts, $content = null) {
@@ -48,7 +33,8 @@ function whatsapp_rituom_shortcode($atts, $content = null) {
     return create_whatsapp_button(
         false,  // Now passing boolean
         whatsapp_url($phone_number, $atts['message']),
-        "WhatsApp Ritu Om"
+        "WhatsApp Ritu Om",
+        true
     );
 }
 
@@ -64,7 +50,8 @@ function whatsapp_program_support_shortcode($atts, $content = null) {
     return create_whatsapp_button(
         false,  // Now passing boolean
         whatsapp_url($phone_number, $atts['message']),
-        "WhatsApp Program Support"
+        "WhatsApp Program Support",
+        false
     );
 }
 
@@ -79,11 +66,10 @@ function whatsapp_group_button_shortcode($atts, $content = null) {
     // Convert string to boolean
     $isAutoClick = filter_var($atts['is-auto-click'], FILTER_VALIDATE_BOOLEAN);
 
-    return create_whatsapp_button($isAutoClick, $atts['url'], $atts['text']);
+    return create_whatsapp_button($isAutoClick, $atts['url'], $atts['text'], false);
 }
 
 // Register the shortcodes
-add_shortcode('whatsapp_param', 'whatsapp_param_shortcode');
 add_shortcode('whatsapp_rituom', 'whatsapp_rituom_shortcode');
 add_shortcode('whatsapp_program_support', 'whatsapp_program_support_shortcode');
 add_shortcode('whatsapp_group_button', 'whatsapp_group_button_shortcode');
